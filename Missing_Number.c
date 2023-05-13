e99bdb2645cc3155182c5926f19a819fd49e3864
Brilliant-Developer1 <email2obail@gmail.com>
2023-05-13 23:59:23 +0600

    file-1

---
+++ Missing_Number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int fun(int s, int a, int b, int c)
+{
+    int sum = a + b + c;
+    int count = 0;
+
+    for (int i = sum; i < s; i++)
+    {
+        count++;
+    }
+
+    // printf("%d %d %d", sum, s, count);
+    return count;
+}
+int main()
+{
+    int input;
+    scanf("%d", &input);
+
+    for (int i = 0; i < input; i++)
+    {
+        int s, a, b, c;
+        scanf("%d %d %d %d", &s, &a, &b, &c);
+        int ans = fun(s, a, b, c);
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
